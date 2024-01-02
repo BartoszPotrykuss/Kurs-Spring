cb8694a8599a8e4a99286e55ea52a8022050fef2
ciornyyy <b.potrykus@o2.pl>
2024-01-02 17:01:52 +0100

    spring boot

---
+++ Spring-boot-tutorial/src/main/java/com/kursdailycodebuffer/Springboot/tutorial/service/DepartmentService.java
@@ -0,0 +1,22 @@
+package com.kursdailycodebuffer.Springboot.tutorial.service;
+
+import com.kursdailycodebuffer.Springboot.tutorial.entity.Department;
+import com.kursdailycodebuffer.Springboot.tutorial.error.DepartmentNotFoundException;
+
+import java.util.List;
+
+public interface DepartmentService {
+
+   public Department saveDepartment(Department department);
+
+   public List<Department> fetchDepartmentList();
+
+   public Department fetchDepartmentById(Long departmentId) throws DepartmentNotFoundException;
+
+   public void deleteDepartmentById(Long departmentId);
+
+   public Department updateDepartment(Long departmentId, Department department);
+
+   public Department fetchDepartmentByName(String departmentName);
+}
+
---
+++ Spring-boot-tutorial/src/test/java/com/kursdailycodebuffer/Springboot/tutorial/SpringBootTutorialApplicationTests.java
@@ -0,0 +1,10 @@
+package com.kursdailycodebuffer.Springboot.tutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTutorialApplicationTests {
+
+
+}
